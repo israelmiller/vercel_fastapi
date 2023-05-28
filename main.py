9acede5bf6a8c81851d73d085fe1796b30bdbeda
Israel Miller <102178821+israelmiller@users.noreply.github.com>
2023-05-28 12:55:05 -0400

    test

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI() # This is what will be refrenced in config
 @app.get('/')
 async def hello():
     return {'res': 'pong', 'version': __version__, "time": time()}
+
+@app.get('/ping')
+async def ping():
+    return {'res': 'pong', 'version': __version__, "time": time()}
